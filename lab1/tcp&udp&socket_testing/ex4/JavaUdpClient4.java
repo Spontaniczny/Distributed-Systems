cf40dde295b4b73d63058b62ad92ed2b69fd0882
Jakub Kasperski <k2002kuba@gmail.com>
2024-03-10 23:33:24 +0100

    Updated project structure

---
+++ lab1/tcp&udp&socket_testing/ex4/JavaUdpClient4.java
@@ -0,0 +1,42 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Arrays;
+
+public class JavaUdpClient4 {
+
+    public static void main(String args[]) throws Exception
+    {
+        System.out.println("JAVA UDP CLIENT");
+        DatagramSocket socket = null;
+        int portNumber = 9008;
+        byte[] receiveBuffer = new byte[1024];
+
+        try {
+            socket = new DatagramSocket();
+            InetAddress address = InetAddress.getByName("localhost");
+            byte[] sendBuffer = "JAVA".getBytes();
+
+            // send message to server
+            DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, address, portNumber);
+            socket.send(sendPacket);
+
+            // receive message from server
+            Arrays.fill(receiveBuffer, (byte)0);
+            DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+            socket.receive(receivePacket);
+
+            // print message from server
+            String msg = new String(receivePacket.getData());
+            System.out.println("received msg: " + msg);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+    }
+}
